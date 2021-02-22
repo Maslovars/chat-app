94aead6e75fb18b325baf55b5db011f9b3c7fc1f
Maslovars <maslovars@gmail.com>
2021-02-22 18:51:06 +0300

    React 05 - State

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,50 @@
 import "./Contact.css";
 import PropTypes from 'prop-types';
+import { Component } from "react";
 
-const Contact = ({ avatar, name, online }) => {
-    return (
-        <div className="Contact">
-            <img className="avatar" src={avatar} alt="userAvatar" />
-            <div>
-                <p className="name">{name}</p>
-                <div className="status">
-                    <span className={online ? "status-online" : "status-offline"}></span>
-                    <p className="status-text">{online ? "Online" : "Offline"}</p>
+// const Contact = ({ avatar, name, online }) => {
+//     return (
+//         <div className="Contact">
+//             <img className="avatar" src={avatar} alt="userAvatar" />
+//             <div>
+//                 <p className="name">{name}</p>
+//                 <div className="status">
+//                     <span className={online ? "status-online" : "status-offline"}></span>
+//                     <p className="status-text">{online ? "Online" : "Offline"}</p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: props.online
+        }
+    }
+    render() {
+        const { avatar, name } = this.props;
+        return (
+            <div className="Contact">
+                <img className="avatar" src={avatar} alt="userAvatar" />
+                <div>
+                    <p className="name">{name}</p>
+                    <div className="status">
+                        <span className={this.state.online ? "status-online" : "status-offline"} onClick={() => {
+                            const newStatus = !this.state.online;
+                            this.setState({ online: newStatus })
+                        }} ></span>
+                        <p className="status-text" onClick={() => {
+                            const newStatus = !this.state.online;
+                            this.setState({ online: newStatus })
+                        }}>{this.state.online ? "Online" : "Offline"}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
 
 Contact.propTypes = {
